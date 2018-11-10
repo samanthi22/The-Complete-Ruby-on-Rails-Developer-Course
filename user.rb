e76566e1d441bbc61ca7cc3cdc16891dfc9e6ac2
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-11-10 21:15:56 +0000

    moved old user.rb up one level

--- user.rb
+++ user.rb
@@ -1,53 +1,75 @@
-class User
-    # pass in name
-    def initialize(name)
-       @name = name 
-    end
-    
-    def run 
-       puts "Hey I'm running" 
-    end
-    
-    def get_name
-        @name
-    end
-    
-    def set_name=(name)
-        @name = name
+module Destructable
+    def destroy(anyobject)
+       puts "I will destroy the object" 
     end
 end
 
-user = User.new("Samanthi")
-user1 = User.new("John")
-user2 = User.new("Kevin")
-puts user.get_name
-user.set_name= "John"
-puts user.get_name
+class User
+    include Destructable
+    
+    attr_accessor :name, :email
+    def initialize(name, email)
+        @name = name
+        @email = email
+    end
+    
+    def run
+        puts "Hey I'm running"
+    end
+    
+    def self.identify_yourself
+        puts "Hey I am a class method"
+    end
+end
 
-# puts User.ancestors
+# < sub-class
+class Buyer < User
+    # inheritance
+    def run
+        puts "Hey I'm not running and I'm in buyer class"
+    end
+end
+
+class Seller < User
+    
+end
+
+class Admin < User
+    
+end
+
+user = User.new("Samanthi", "sjinadasa@fas.harvard.edu")
+puts "My user's name is #{user.name} and her email is #{user.email}"
+user.name = "John"
+user.email = "john@example.com"
+puts user.name
+puts "My user's new name is #{user.name} and his email is #{user.email}"
+
+buyer1 = Buyer.new("John Doe", "johndoe@example.com")
+buyer1.run
+seller1 = Seller.new("John Doe1", "johndoe1@example.com")
+seller1.run
+admin1 = Admin.new("John Doe2", "johndoe2@example.com")
+admin1.run
+puts Buyer.ancestors
+
+User.identify_yourself
 =begin
+My user's name is Samanthi and her email is sjinadasa@fas.harvard.edu
+John
+My user's new name is John and his email is john@example.com
+Hey I'm not running and I'm in buyer class
+Hey I'm running
+Hey I'm running
+Buyer
+User
 Object
 Kernel
 BasicObject
-#<User:0x0000000173e4d8>
+Hey I am a class method
 =end
-=begin
-#<User:0x00000001271e00>
-#<User:0x00000001271d88>
-#<User:0x00000001271e50>
-=end
-puts user1
-puts user2
-user.run
-user1.run
-user2.run
 
-puts user 
-# Object gets created
-
-# getter method
-puts user.get_name
-# output
-# user.rb:37:in `<main>': undefined method `name' for #<User:0x0000000159d930 @name="Samanthi"> (NoMethodError)
-
-# get name
+user = User.new("jinadasa", "sjinadasa@fas.harvard.edu")
+# user2.rb:72:in `<main>': undefined method `destroy' for #<User:0x000000020a03c8> (NoMethodError)
+# this method is not accessible by user
+user.destroy("myname")
