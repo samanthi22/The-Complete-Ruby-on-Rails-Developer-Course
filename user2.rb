ce3715da4cb3df7d7f959d674befc9c34e89ef35
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2018-11-10 20:14:43 +0000

    #{user.name

--- user2.rb
+++ user2.rb
@@ -11,9 +11,25 @@ class User
     end
 end
 
+# < sub-class
+class Buyer < User
+    
+end
+
+class Seller < User
+    
+end
+
+class Admin < User
+    
+end
+
 user = User.new("Samanthi", "sjinadasa@fas.harvard.edu")
-puts "My user's name is #{user.name} and his email is #{user.email}"
+puts "My user's name is #{user.name} and her email is #{user.email}"
 user.name = "John"
 user.email = "john@example.com"
 puts user.name
 puts "My user's new name is #{user.name} and his email is #{user.email}"
+
+buyer1 = Buyer.new("John Doe", "johndoe@example.com")
+buyer1.run
