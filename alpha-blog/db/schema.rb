1de5b38bb64bad975964149da2007acdc1c034b8
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2020-01-18 16:21:43 +0000

    add_description_to_articles

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190604132545) do
+ActiveRecord::Schema.define(version: 20190604132134) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description"
-    t.datetime "create_at"
-    t.datetime "updated_at"
   end
 
 end
